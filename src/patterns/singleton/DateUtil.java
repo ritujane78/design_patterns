460825717b4783590c19b6aa898d2351740fff6f
Ritu Bafna <ritujane78@gmail.com>
2026-09-19 13:38:01 +0545

    fist commit

---
+++ src/patterns/singleton/DateUtil.java
@@ -0,0 +1,12 @@
+package patterns.singleton;
+
+public class DateUtil {
+  private static DateUtil instance;
+  private DateUtil(){
+
+  }
+  public static DateUtil getInstance(){
+    if(instance == null) instance = new DateUtil();
+    return instance;
+  }
+}
