690fc86a4c1b4027e0546466f560d9633d5ad98e
Ritu Bafna <ritujane78@gmail.com>
2026-09-20 12:01:07 +0545

    create one instance of the object when serialising and  deserialising it

--- src/patterns/singleton/DateUtil.java
+++ src/patterns/singleton/DateUtil.java
@@ -1,14 +1,25 @@
 package patterns.singleton;
 
-public class DateUtil {
-  private static DateUtil instance;
-  static {
-    instance = new DateUtil();
-  }
+import java.io.Serializable;
+
+public class DateUtil implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private static volatile DateUtil instance;
   private DateUtil(){
 
   }
   public static DateUtil getInstance(){
+    if (instance == null){
+      synchronized (DateUtil.class) {
+        if (instance == null) instance = new DateUtil();
+      }
+    }
+
+    return instance;
+  }
+  public Object readResolve(){
     return instance;
   }
 }
--- src/patterns/singleton/Test.java
+++ src/patterns/singleton/Test.java
@@ -1,9 +1,18 @@
 package patterns.singleton;
 
+import java.io.*;
+
 public class Test {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
     DateUtil dateUtil1 = DateUtil.getInstance();
-    DateUtil dateUtil2 = DateUtil.getInstance();
+    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("C:\\Users\\rituj\\Documents\\Coding\\corepatterns\\singleton\\DateUtil.ser"));
+    oos.writeObject(dateUtil1);
+    DateUtil dateUtil2 = null;
+    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\Users\\rituj\\Documents\\Coding\\corepatterns\\singleton\\DateUtil.ser"));
+    dateUtil2 = (DateUtil) ois.readObject();
+
+    oos.close();
+    ois.close();
     System.out.println(dateUtil1 == dateUtil2);
 
   }
